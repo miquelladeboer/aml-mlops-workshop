573ca6cc86d78269bfb8baa9d9ae9468da0efe30
Dennis Eikelenboom <mail@denniseik.nl>
2020-05-07 10:16:49 +0200

    download model outputs

--- code/build-and-release/scripts/download_model.py
+++ code/build-and-release/scripts/download_model.py
@@ -1,4 +1,7 @@
 import argparse
+from azureml.core import Workspace
+from azureml.core.authentication import AzureCliAuthentication
+from azureml.core.model import Model
 
 
 # Parse Definition Arguments
@@ -8,12 +11,12 @@ parser.add_argument(
     "--name",
     dest='name',
     type=str,
-    help='The model name.'
+    help='The model name'
 )
 parser.add_argument(
     "-v",
     "--version",
-    dest='model version',
+    dest='version',
     type=int,
     help='The model version'
 )
@@ -29,3 +32,21 @@ args = parser.parse_args()
 print("Model name: ", args.name)
 print("Model version: ", args.version)
 print("Output dir: ", args.output_dir)
+
+# Connect to the AML Workspace
+ws = Workspace.from_config(
+    auth=AzureCliAuthentication()
+)
+
+# Retrieve the model
+model = Model(
+    workspace=ws,
+    name=args.name,
+    version=args.version
+)
+
+# Download model to target dir
+model.download(
+    target_dir=args.output_dir,
+    exist_ok=True
+)
